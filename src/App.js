e7c2228c15c1247d2290b35485280f9e213760bb
ian <developer.kss@gmail.com>
2019-06-03 23:27:52 +0900

    formatting

--- src/App.js
+++ src/App.js
@@ -6,50 +6,51 @@ import Movie from './Movie';
 
 class App extends Component {
 
-  state = {
-    greeting:"Hello Movie",
-  }
+    state = {
+        greeting: "Hello Movie",
+    }
 
-  componentWillMount() {
-    console.log('will mount')
-  }
+    componentWillMount() {
+        console.log('will mount')
+    }
 
-  componentDidMount() {
-    this._getMovies();
-  }
+    componentDidMount() {
+        this._getMovies();
+    }
 
-  _getMovies = async() => {
-    const movies = await this._callApi()
-    this.setState({
-      movies:movies
-    })
-  }
+    _getMovies = async () => {
+        const movies = await this._callApi()
+        this.setState({
+            movies: movies
+        })
+    }
 
-  _callApi = () => {
-    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count")
-    .then(response=>response.json())
-    .then(json=>{
-      console.log(json.data)
-      return json.data.movies}
-      )
-    .catch(err=>console.log(err))
-  }
+    _callApi = () => {
+        return fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count")
+            .then(response => response.json())
+            .then(json => {
+                    console.log(json.data)
+                    return json.data.movies
+                }
+            )
+            .catch(err => console.log(err))
+    }
 
-  _renderMovies = () => {
-    return this.state.movies.map(movie=> {
-      return <Movie title={movie.title} image={movie.large_cover_image} key={movie.id}/>
-    })
-  }
+    _renderMovies = () => {
+        return this.state.movies.map(movie => {
+            return <Movie title={movie.title} image={movie.large_cover_image} key={movie.id}/>
+        })
+    }
 
-  render() {
-    return (
-      <div className="App">
-        {this.state.movies ? this._renderMovies() : "Loading..."}
-        
-      </div>
-    );
-  }
-  
+    render() {
+        return (
+            <div className="App">
+                {this.state.movies ? this._renderMovies() : "Loading..."}
+
+            </div>
+        );
+    }
+
 }
 
 
--- src/Movie.js
+++ src/Movie.js
@@ -5,10 +5,10 @@ import PropTypes from 'prop-types'
 function Movie({title, image}, props) {
     return (
         <div>
-        <MoviePoster image={image}/>
-        <h1>{title}</h1>
-    </div>
-)
+            <MoviePoster image={image}/>
+            <h1>{title}</h1>
+        </div>
+    )
 }
 
 Movie.propTypes = {
@@ -19,11 +19,11 @@ Movie.propTypes = {
 
 function MoviePoster({image}, props) {
     return (
-            <img width="100" height="auto" src={image} />
+        <img width="100" height="auto" src={image}/>
     )
 }
 
 MoviePoster.propTypes = {
-    image:PropTypes.string.isRequired
+    image: PropTypes.string.isRequired
 }
 export default Movie;
